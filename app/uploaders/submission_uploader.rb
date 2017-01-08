1495bfda907ebfe9e5e3c1585ea6dd275d67a2ee
vit <shiegin@gmail.com>
2017-01-08 18:24:47 +0300

    change directories counter modulo

--- app/uploaders/submission_uploader.rb
+++ app/uploaders/submission_uploader.rb
@@ -12,7 +12,7 @@ class SubmissionUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id % 10}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id % 1000}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
