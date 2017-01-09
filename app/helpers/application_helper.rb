516d96fb5b8278d5c7d3189eb5e7dcff322bc417
vit <shiegin@gmail.com>
2017-01-09 06:01:49 +0300

    fixed #44

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+include ActionView::Helpers::TagHelper
+
+def translate_with_link(key, *urls)
+  urls.inject(I18n.t(key)) { |s, url|
+    s.sub(/\*\[(.+?)\]/, content_tag(:a, '\1', :href => url)).html_safe
+  }
+end
+
 end
