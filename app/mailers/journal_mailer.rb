afd8bd7a6f7790854e425959d605bc9ff27e0ff8
vit <shiegin@gmail.com>
2017-11-06 01:52:46 +0300

    updated many emails

--- app/mailers/journal_mailer.rb
+++ app/mailers/journal_mailer.rb
@@ -7,6 +7,10 @@ class JournalMailer < ApplicationMailer
 	# on submit
     def submission_submitted_author(submission)
         @submission = submission
+
+        @authors_text = @submission.get_authors_submitted.map{ |a| a.full_name }.join(', ')
+        @title_text = @submission.get_text_submitted.title rescue ''
+
         @journal = @submission.journal
         @user = @submission.user
         @submission_url = submission_url(submission)
@@ -14,12 +18,14 @@ class JournalMailer < ApplicationMailer
         n = @submission.revisions.count
 
         if n<2
-            subject = "#{@journal.slug}##{submission.id} New paper submitted | Новая статья подана"
+#            subject = "#{@journal.slug}##{submission.id} New paper submitted | Новая статья подана"
+            subject = "##{@submission.id} You submitted the paper. Journal Gyroscopy and Navigation | Вы подали статью. Журнал \"Гироскопия и навигация\""
     		mail(to: @user.email, subject: subject) do |format|
                 format.text { render 'submission_submitted_author_first' }
 		    end
         else
-            subject = "#{@journal.slug}##{submission.id} New version submitted | Новая версия подана"
+#            subject = "#{@journal.slug}##{submission.id} New version submitted | Новая версия подана"
+            subject = "##{@submission.id} You submitted new version. Journal Gyroscopy and Navigation | Вы подали новую версию. Журнал \"Гироскопия и навигация\""
     		mail(to: @user.email, subject: subject) do |format|
                 format.text { render 'submission_submitted_author_second' }
 		    end
@@ -27,17 +33,23 @@ class JournalMailer < ApplicationMailer
     end
     def submission_submitted_editor(submission, user)
         @submission = submission
+
+        @authors_text = @submission.get_authors_submitted.map{ |a| a.full_name }.join(', ')
+        @title_text = @submission.get_text_submitted.title rescue ''
+
         @journal = @submission.journal
         @submission_editor_url = e_submission_url(submission)
         n = @submission.revisions.count
             @user = user
             if n<2
-                subject = "#{@journal.slug}##{submission.id} New paper submitted | Новая статья подана"
+#                subject = "#{@journal.slug}##{submission.id} New paper submitted | Новая статья подана"
+                subject = "##{@submission.id} New paper. Journal Gyroscopy and Navigation | Новая статья. Журнал \"Гироскопия и навигация\""
         		mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_submitted_editor_first' }
 		        end
             else
-                subject = "#{@journal.slug}##{submission.id} New version submitted | Новая версия подана"
+#                subject = "#{@journal.slug}##{submission.id} New version submitted | Новая версия подана"
+                subject = "##{@submission.id} New version. Journal Gyroscopy and Navigation | Новая версия. Журнал \"Гироскопия и навигация\""
     		    mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_submitted_editor_second' }
 	    	    end
@@ -45,6 +57,12 @@ class JournalMailer < ApplicationMailer
     end
     def submission_submitted_reviewer(submission, user)
         @submission = submission
+
+        @invitation = @submission.user_invitation user
+
+        @authors_text = @submission.get_authors_submitted.map{ |a| a.full_name }.join(', ')
+        @title_text = @submission.get_text_submitted.title rescue ''
+
         @journal = @submission.journal
         @submission_reviewer_url = r_submission_url(submission)
         n = @submission.revisions.count
@@ -52,7 +70,8 @@ class JournalMailer < ApplicationMailer
 
             if n<2
             else
-                subject = "#{@journal.slug}##{submission.id} New version submitted | Новая версия подана"
+#                subject = "#{@journal.slug}##{submission.id} New version submitted | Новая версия подана"
+                subject = "##{@submission.id} New version. Journal Gyroscopy and Navigation | Новая версия. Журнал \"Гироскопия и навигация\""
     		    mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_submitted_reviewer_second' }
 	    	    end
@@ -63,6 +82,10 @@ class JournalMailer < ApplicationMailer
 	# on decision
     def submission_decision_author(submission)
         @submission = submission
+
+        @authors_text = @submission.get_authors_submitted.map{ |a| a.full_name }.join(', ')
+        @title_text = @submission.get_text_submitted.title rescue ''
+
         @journal = @submission.journal
         @user = @submission.user
         @submission_url = submission_url(submission)
@@ -74,27 +97,32 @@ class JournalMailer < ApplicationMailer
         if @decision
             case @decision.decision
             when 'take_for_consideration'
-                subject = "#{@journal.slug}##{submission.id} The paper was taken for consideration | Статья принята к рассмотрению"
+#                subject = "#{@journal.slug}##{submission.id} The paper was taken for consideration | Статья принята к рассмотрению"
+                subject = "##{@submission.id} Taken for consideration. Journal Gyroscopy and Navigation | Принято к рассмотрению. Журнал \"Гироскопия и навигация\""
         		mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_decision_author_take_for_consideration' }
 		        end
             when 'reject_without_consideration'
-                subject = "#{@journal.slug}##{submission.id} The paper was rejected without consideration | Статья отклонена без рассмотрения"
+#                subject = "#{@journal.slug}##{submission.id} The paper was rejected without consideration | Статья отклонена без рассмотрения"
+                subject = "##{@submission.id} Rejected without consideration. Journal Gyroscopy and Navigation | Отклонено без рассмотрения. Журнал \"Гироскопия и навигация\""
         		mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_decision_author_reject_without_consideration' }
 		        end
             when 'reject'
-                subject = "#{@journal.slug}##{submission.id} The paper was rejected | Статья отклонена"
+#                subject = "#{@journal.slug}##{submission.id} The paper was rejected | Статья отклонена"
+                subject = "##{@submission.id} Paper rejected. Journal Gyroscopy and Navigation | Статья отклонена. Журнал \"Гироскопия и навигация\""
         		mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_decision_author_reject' }
 		        end
             when 'revise'
-                subject = "#{@journal.slug}##{submission.id} The paper must be revised | Статью надо доработать"
+#                subject = "#{@journal.slug}##{submission.id} The paper must be revised | Статью надо доработать"
+                subject = "##{@submission.id} Paper must be revised. Journal Gyroscopy and Navigation | Статью надо доработать. Журнал \"Гироскопия и навигация\""
         		mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_decision_author_revise' }
 		        end
             when 'accept'
-                subject = "#{@journal.slug}##{submission.id} The paper was accepted | Статья принята"
+#                subject = "#{@journal.slug}##{submission.id} The paper was accepted | Статья принята"
+                subject = "##{@submission.id} Paper accepted. Journal Gyroscopy and Navigation | Статья принята. Журнал \"Гироскопия и навигация\""
         		mail(to: @user.email, subject: subject) do |format|
                     format.text { render 'submission_decision_author_accept' }
 		        end
@@ -217,18 +245,24 @@ class JournalMailer < ApplicationMailer
     def submission_invitation_decision_editor invitation, user
         @invitation = invitation
         @submission = @invitation.submission
+
+        @authors_text = @submission.get_authors_submitted.map{ |a| a.full_name }.join(', ')
+        @title_text = @submission.get_text_submitted.title rescue ''
+
         @journal = @submission.journal
         @user = user
         @reviewer = @invitation.user
         @submission_editor_url = e_submission_url(@submission)
         @submission_reviewer_url = r_submission_url(@submission)
         if @invitation.accepted?
-            subject = "#{@journal.slug}##{@submission.id} Reviewer invitation was accepted | Рецензент принял приглашение"
+#            subject = "#{@journal.slug}##{@submission.id} Reviewer invitation was accepted | Рецензент принял приглашение"
+            subject = "##{@submission.id} Invitation accepted. Journal Gyroscopy and Navigation | Приглашение принято. Журнал \"Гироскопия и навигация\""
         	mail(to: @user.email, subject: subject) do |format|
                 format.text { render 'submission_invitation_decision_editor_accepted' }
             end
         elsif @invitation.declined?
-            subject = "#{@journal.slug}##{@submission.id} Reviewer invitation was declined | Рецензент отклонил приглашение"
+#            subject = "#{@journal.slug}##{@submission.id} Reviewer invitation was declined | Рецензент отклонил приглашение"
+            subject = "##{@submission.id} Invitation declined. Journal Gyroscopy and Navigation | Приглашение отклонено. Журнал \"Гироскопия и навигация\""
         	mail(to: @user.email, subject: subject) do |format|
                 format.text { render 'submission_invitation_decision_editor_rejected' }
             end
@@ -239,6 +273,10 @@ class JournalMailer < ApplicationMailer
         @review = review
         @revision = @review.revision
         @submission = @revision.submission
+
+        @authors_text = @submission.get_authors_submitted.map{ |a| a.full_name }.join(', ')
+        @title_text = @submission.get_text_submitted.title rescue ''
+
         @journal = @submission.journal
         @user = user
         @reviewer = @review.user
